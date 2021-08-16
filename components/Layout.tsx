c61ff0abd6970b6c67ca08d15c48ca231411a56d
hotake <hotake@MacBook-Air.local>
2021-08-16 10:54:25 +0900

    add blog title

---
+++ components/Layout.tsx
@@ -0,0 +1,26 @@
+import React, { ReactFragment } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import Nav from '../components/nav';
+interface LayoutProps {
+    children: React.ReactNode;
+ }
+
+export default function Layout({ children }:LayoutProps) {
+    return (
+        <>
+            <Head>
+                <title>hotakesBlog</title>
+            </Head>
+            <a>
+                <h1>
+                    <Link href="/" passHref>
+                        HOTAKES BLOG
+                    </Link>
+                </h1>
+            </a>
+            <Nav />
+            {children}
+        </>
+        )
+  }
