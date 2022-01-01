3e0d9f0368d4920e35d9a8ab8a757836a9c49132
ifhito <hito01010101@gmail.com>
2022-01-01 10:06:16 +0900

    mainタグを正常の部分に移動

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,46 +13,38 @@ const BlogItem: React.FC<BlogItemType> = props => {
 
   return (
           <>
-            <HeadWrapper
-            title='Contents'
-            description='ブログコンテンツのリストページ'
-            url={__filename}
-            type='blog'
-            />
-            <main id='#main'>
-              <li id="page-li" className="content-color">
-                <h2>
-                  <Link href="/blogs/[id]" as={`/blogs/${item.id}`} passHref>
-                    <a href="replace" className="content-color link-border-none">
-                      { item.title }
-                    </a>
-                  </Link>
-                </h2>
-              <span id="date" className="content-color">{ formatDate(item.date) }</span>
-              </li>
-              <style jsx global>{`
-                h2 {
-                  margin:0;
-                  border-bottom: 3px solid;
-                  line-height: 1.5;
-                }
-                a {
-                  width:100%;
-                }
-                #page-li {
-                  margin-bottom: 2.4rem;
-                }
-                @media (max-width: 767px){
-                  // #date {
-                  //   font-size: 20px
-                  // }
-                  // h2 {
-                  //   margin:0;
-                  //   font-size: 50px;
-                  // }
-                }
-              `}</style>
-            </main>
+            <li id="page-li" className="content-color">
+              <h2>
+                <Link href="/blogs/[id]" as={`/blogs/${item.id}`} passHref>
+                  <a href="replace" className="content-color link-border-none">
+                    { item.title }
+                  </a>
+                </Link>
+              </h2>
+            <span id="date" className="content-color">{ formatDate(item.date) }</span>
+            </li>
+            <style jsx global>{`
+              h2 {
+                margin:0;
+                border-bottom: 3px solid;
+                line-height: 1.5;
+              }
+              a {
+                width:100%;
+              }
+              #page-li {
+                margin-bottom: 2.4rem;
+              }
+              @media (max-width: 767px){
+                // #date {
+                //   font-size: 20px
+                // }
+                // h2 {
+                //   margin:0;
+                //   font-size: 50px;
+                // }
+              }
+            `}</style>
           </>
   )
 }
@@ -64,42 +56,52 @@ const Blogs: NextPage = (props: any) => {
     setEndContent(endContent+5)
   }
   return (
-    <div className="blog-container">
-      <ul>
-      {
-        contents.slice(0,endContent).map( (item: { id: React.Key; }) => <BlogItem items={ item } key={ item.id } />)
-      }
-      </ul>
-      {
-        contents.length > endContent ? (
-          <div id="button-wrapper">
-            <button type="button" id="more-button" onClick={handlerClick}>もっと見る</button>
-          </div>
-        ):''
-      }
-      <style jsx global>{`
-        #more-button {
-          background-color: transparent;
-          color: #2196F3;
-          border-color: #CDDC39;
-          border-radius: 0.9em;
-          border-style: solid;
-          font-size: 2.4rem;
-          padding: 0.1rem 5rem;
-        }
-        #more-button:focus {
-          background-color: #cddc39;
-          color: #252525;
-        }
-        #more-button:hover {
-          background-color: #cddc39;
-          color: #252525;
-        }
-        #button-wrapper {
-          text-align:center;
-        }
-      `}</style>
-    </div>
+    <>
+      <HeadWrapper
+        title='Contents'
+        description='ブログコンテンツのリストページ'
+        url={__filename}
+        type='blog'
+      />
+      <main id='main'>
+        <div className="blog-container">
+          <ul>
+          {
+            contents.slice(0,endContent).map( (item: { id: React.Key; }) => <BlogItem items={ item } key={ item.id } />)
+          }
+          </ul>
+          {
+            contents.length > endContent ? (
+              <div id="button-wrapper">
+                <button type="button" id="more-button" onClick={handlerClick}>もっと見る</button>
+              </div>
+            ):''
+          }
+          <style jsx global>{`
+            #more-button {
+              background-color: transparent;
+              color: #2196F3;
+              border-color: #CDDC39;
+              border-radius: 0.9em;
+              border-style: solid;
+              font-size: 2.4rem;
+              padding: 0.1rem 5rem;
+            }
+            #more-button:focus {
+              background-color: #cddc39;
+              color: #252525;
+            }
+            #more-button:hover {
+              background-color: #cddc39;
+              color: #252525;
+            }
+            #button-wrapper {
+              text-align:center;
+            }
+          `}</style>
+        </div>
+      </main>
+    </>
   )
 }
 
