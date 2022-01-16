5e2ae548225ad50691fabc8b05b514f6990d4e50
ifhito <hito01010101@gmail.com>
2022-01-16 22:23:18 +0900

    delete code

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,6 @@ const Blogs: NextPage = (props: any) => {
   const handleSetContents = (e:any)=>{
     if(e.target.value === 'all'){
       category.current = e.target.value;
-      window.location.hash = '';
       setCurrentContents(contents);
     }else{
       category.current = e.target.value;
