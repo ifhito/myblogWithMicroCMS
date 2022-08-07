02142740e9dbb42416d022b205c7fb81534021bc
ifhito <hito01010101@gmail.com>
2022-08-07 11:34:59 +0900

    fix introduction sentence in about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,5 @@
-import React, { ReactFragment } from 'react';
-import HeadWrapper from '../components/HeadWrapper';
+import React, { ReactFragment } from "react";
+import HeadWrapper from "../components/HeadWrapper";
 
 const About: ReactFragment = () => {
   return (
@@ -11,34 +11,45 @@ const About: ReactFragment = () => {
         type='article'
       />
       <main id='main'>
-        <h1 className="head-color">ブログ著者と自己紹介</h1>
-        <h2 className="content-color">著者: HOTAKE</h2>
-        <p className="content-color">
-            東京のWeb企業(なのだろうか?)でエンジニアをやっている人です
+        <h1 className='head-color'>ブログ著者と自己紹介</h1>
+        <h2 className='content-color'>著者: HOTAKE</h2>
+        <p className='content-color'>
+          東京のWeb企業(なのだろうか?)でエンジニアをやっている人です
         </p>
-        <p className="content-color">
-            実務では、運用でインフラ系ばっかやっています…(Terraform、AWS)
+        <p className='content-color'>
+          実務では、運用でインフラ系Terraform、AWS、ニフクラ、サーバー内作業)をやったりRailsアプケーションの改修作業を行うなどしています。
         </p>
-        <p className="content-color">
-            ただ、フロントエンドに興味があり、フロントエンドに触れたいため、アクセシビリティやReact、Nextなどについて、勉強しています。
+        <p>
+          最近はやっとVue.jsやReactを業務で触れたりするようになりましたが、社内ツール作成で使うだけです。
         </p>
-        <h2 className="content-color">技術スタックなど</h2>
-        <h3 className="content-color">言語</h3>
-        <ul className="content-color list-ul">
+        <p className='content-color'>
+          フロントエンドに興味があり、フロントエンドに触れたいため、アクセシビリティやReact、Nextなどについて、個人では勉強しています。
+        </p>
+        <p>
+          アクセシビリティがやりたくて社内でオーサリングプラクティスの輪読会を開きましたが、全く人気がなく、3人しか参加者が集まりませんでした(泣)。普通にアクセシビリティ勉強会にしたら人が来たのかななどと思ったりもします。ただ、アクセシビリティには実践がないとなとも思うのでどうすれば良いのかと思っています。
+        </p>
+        <h2 className='content-color'>技術スタックなど</h2>
+        <h3 className='content-color'>言語</h3>
+        <ul className='content-color list-ul'>
           <li>HTML/CSS</li>
           <li>JavaScript</li>
           <li>TypeScript</li>
           <li>Python</li>
           <li>Rust</li>
           <li>PHP</li>
+          <li>Ruby</li>
         </ul>
-        <h3 className="content-color">ライブラリ</h3>
-        <ul className="content-color list-ul">
+        <h3 className='content-color'>ライブラリ</h3>
+        <ul className='content-color list-ul'>
           <li>React</li>
+          <li>Vue.js</li>
           <li>Flask</li>
+          <li>Rails</li>
         </ul>
-        <h3 className="content-color">その他</h3>
-        <ul className="content-color list-ul">
+        <h3 className='content-color'>その他</h3>
+        <ul className='content-color list-ul'>
+          <li>アクセシビリティ</li>
+          <li>スクラム開発</li>
           <li>webpack</li>
           <li>Terraform</li>
           <li>Docker</li>
@@ -48,11 +59,11 @@ const About: ReactFragment = () => {
       </main>
       <style jsx>{`
         .list-ul {
-            list-style: inside;
+          list-style: inside;
         }
       `}</style>
     </>
-  )
-}
+  );
+};
 
 export default About;
