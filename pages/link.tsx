d241a1a6e63f16c02b2cf292b8f842ef3b270c22
ifhito <hito01010101@gmail.com>
2022-08-07 11:48:04 +0900

    fix list to link

--- pages/link.tsx
+++ pages/link.tsx
@@ -1,36 +1,44 @@
-import React, { ReactFragment } from 'react';
+import React, { ReactFragment } from "react";
 import Link from "next/link";
-import HeadWrapper from '../components/HeadWrapper';
+import HeadWrapper from "../components/HeadWrapper";
 
 const Link_: ReactFragment = () => {
-    const urls = ['https://twitter.com/ifhito', 'https://note.com/ifhito', 'https://github.com/ifhito', 'https://zenn.dev/ifhito', 'https://bookmeter.com/users/991028']
-    return (
-        <>
-            <HeadWrapper
-                title='List'
-                description='別リンクへのページ'
-                url={__filename}
-                type='article'
-            />
-            <main id='main'>
-                <h1 className="head-color">その他リンク</h1>
-                <ul>
-                    {urls.map(url => (
-                        <li id='url-link' key={url.split('.')[0].split('//')[1]}>
-                            <Link href={url} passHref>
-                                <a href="replace" className="content-color">{url.split('.')[0].split('//')[1]}</a>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-                <style jsx>{`
-                    #url-link {
-                        margin-bottom: 20px;
-                    }
-                `}</style>
-            </main>
-        </>
-    )
-}
+  const urls = [
+    "https://twitter.com/ifhito",
+    "https://note.com/ifhito",
+    "https://github.com/ifhito",
+    "https://zenn.dev/ifhito",
+    "https://bookmeter.com/users/991028",
+  ];
+  return (
+    <>
+      <HeadWrapper
+        title='Link'
+        description='別リンクへのページ'
+        url={__filename}
+        type='article'
+      />
+      <main id='main'>
+        <h1 className='head-color'>その他リンク</h1>
+        <ul>
+          {urls.map((url) => (
+            <li id='url-link' key={url.split(".")[0].split("//")[1]}>
+              <Link href={url} passHref>
+                <a href='replace' className='content-color'>
+                  {url.split(".")[0].split("//")[1]}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <style jsx>{`
+          #url-link {
+            margin-bottom: 20px;
+          }
+        `}</style>
+      </main>
+    </>
+  );
+};
 
 export default Link_;
